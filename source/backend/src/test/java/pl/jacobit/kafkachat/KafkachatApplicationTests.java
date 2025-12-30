62dd398257d02038bd0d5ae033883e97f349254a
jakub <jacob_the_liar@tlen.pl>
2025-12-30 13:47:35 +0100

    feat(backend): initialize Maven-based Spring Boot project for Kafka Chat
    - Add Maven wrapper scripts with configuration.
    - Configure `.gitignore` for IntelliJ, NetBeans, VS Code, and Maven-related files.
    - Include `application.properties` with `spring.application.name=Kafka Chat`.
    - Add `pom.xml` with dependencies for Spring Boot and Lombok.
    - Create initial application entry point `KafkaChatApplication`.
    - Setup basic test structure with `KafkachatApplicationTests`.

---
+++ source/backend/src/test/java/pl/jacobit/kafkachat/KafkachatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacobit.kafkachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
