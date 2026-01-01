7009d888ac0fca3920a6727544b80e23de027a2c
jakub <jacob_the_liar@tlen.pl>
2026-01-01 20:07:28 +0100

    feat(frontend): implement WebSocket service with message handling and state management
    - Create `WebSocket` service to manage WebSocket connections, message sending, and state tracking (e.g., connection status and current room).
    - Add `ChatMessage` and `RoomData` models to standardize data structures for chat communication.
    - Introduce support for observables to emit chat messages and signals to manage connection state.

---
+++ source/frontend/src/app/model/chat-message.model.ts
@@ -0,0 +1,6 @@
+export interface ChatMessage {
+  username: string;
+  roomName: string;
+  message: string;
+  timestamp: string
+}
---
+++ source/frontend/src/app/service/web-socket.service.ts
@@ -0,0 +1,77 @@
+import {Injectable, signal} from '@angular/core';
+import {Client} from '@stomp/stompjs';
+import {Subject} from 'rxjs';
+import {ChatMessage} from '../model/chat-message.model';
+import {RoomData} from '../model/room-data.model';
+import SockJS from 'sockjs-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebSocket {
+
+  private stompClient?: Client
+
+  private messagesSubject = new Subject<ChatMessage>();
+  public messages$ = this.messagesSubject.asObservable();
+
+  public isConnected = signal<boolean>(false);
+  public currentRoom = signal<string | null>(null);
+
+
+  connect(roomData: RoomData){
+    this.stompClient = new Client({
+      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+
+      onConnect: () => {
+        if (this.stompClient) {
+          const { roomName } = roomData;
+          this.stompClient.subscribe(`/topic/room/${roomName}`, (message) => {
+            const chatMessage: ChatMessage = JSON.parse(message.body);
+            this.messagesSubject.next(chatMessage); // emit do Observable
+          });
+
+          this.isConnected.set(true);
+          this.currentRoom.set(roomName);
+        }
+      },
+
+      onStompError: (frame) => {
+        console.error('STOMP Error:', frame);
+      },
+
+      onWebSocketError: (error) => {
+        console.error('WebSocket Error:', error);
+      },
+
+    });
+
+    this.stompClient.activate();
+  }
+
+  sendMessage(username: string, message: string): void {
+    if (!this.stompClient?.connected || !this.currentRoom()) {
+      console.error('Not connected to WebSocket');
+      return;
+    }
+
+    const payload = {
+      roomName: this.currentRoom(),
+      username: username,
+      message: message
+    };
+
+    this.stompClient.publish({
+      destination: '/app/chat.send',
+      body: JSON.stringify(payload)
+    });
+  }
+
+  disconnect(): void {
+    if (this.stompClient) {
+      this.stompClient.deactivate()
+        .then(() => this.currentRoom.set(null))
+        .finally(() => this.isConnected.set(false));
+    }
+  }
+}
