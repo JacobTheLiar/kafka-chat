1dc0f2a1376cb9fef13ef4e78865134d9b3024da
jakub <jacob_the_liar@tlen.pl>
2026-01-01 18:36:19 +0100

    feat(backend): add KafkaConsumerService to process and forward chat messages
    - Implement `KafkaConsumerService` to consume messages from the "chat-messages" Kafka topic.
    - Forward received messages to appropriate WebSocket topic using `SimpMessagingTemplate`.
    - Log message details including room, user, and message for debugging purposes.

---
+++ source/backend/src/main/java/pl/jacobit/kafkachat/service/KafkaConsumerService.java
@@ -0,0 +1,27 @@
+package pl.jacobit.kafkachat.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import pl.jacobit.kafkachat.model.ChatMessage;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaConsumerService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @KafkaListener(topics = "chat-messages", groupId = "chat-group")
+    public void consumeMessage(ConsumerRecord<String, ChatMessage> consumedRecord) {
+        var roomName = consumedRecord.key();
+        var message = consumedRecord.value();
+
+        log.info("Received from Kafka - Room: {}, User: {}, Message: {}", roomName, message.username(), message.message());
+
+        messagingTemplate.convertAndSend("/topic/room/" + roomName, message);
+    }
+}
