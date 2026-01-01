034efff2141559b0d7a7a10683fe15423f874070
jakub <jacob_the_liar@tlen.pl>
2026-01-01 19:20:44 +0100

    feat(frontend): integrate WebSocket dependencies and update components
    - Add `@stomp/stompjs`, `sockjs-client`, and related type definitions to project dependencies.
    - Remove placeholder content from `app.html` and update to include `<app-join-form>` and `<app-chat-room>` components.
    - Update `app.ts` to import and register `JoinForm` and `ChatRoom` components.
    - Modify `pnpm-lock.yaml` and `package.json` to reflect added dependencies.

--- source/frontend/src/app/app.ts
+++ source/frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {JoinForm} from './components/join-form/join-form';
+import {ChatRoom} from './components/chat-room/chat-room';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JoinForm, ChatRoom],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
 }
