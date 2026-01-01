fd17b9715dc644749c294f1e810d2a55b4dcd982
jakub <jacob_the_liar@tlen.pl>
2026-01-01 20:53:25 +0100

    feat(frontend): build real-time chat-room component with message display and input handling
    - Add `username` and `roomName` inputs for dynamic data binding.
    - Display chat messages with sender, content, and formatted timestamp.
    - Implement message input field with two-way binding and send functionality.
    - Integrate WebSocket service for real-time message handling and updates.
    - Enhance UI with Angular features like `ngFor`, signals, and reactive model updates.

--- source/frontend/src/app/components/chat-room/chat-room.ts
+++ source/frontend/src/app/components/chat-room/chat-room.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, inject, input, model, OnInit, signal} from '@angular/core';
+import {ChatMessage} from '../../model/chat-message.model';
+import {ChatWebSocket} from '../../service/web-socket.service';
+import {FormsModule} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-chat-room',
-  imports: [],
+  imports: [
+    FormsModule,
+    DatePipe
+  ],
   templateUrl: './chat-room.html',
   styleUrl: './chat-room.scss',
 })
-export class ChatRoom {
+export class ChatRoom implements OnInit{
+
+  protected readonly username = input.required<string>();
+  protected readonly roomName = input.required<string>();
+
+  private readonly webSocketService = inject(ChatWebSocket);
+
+  protected messages = signal<ChatMessage[]>([]);
+  protected currentMessage = model('');
+
+  ngOnInit() {
+    this.webSocketService.messages$.subscribe(message => {
+      this.messages.update(messages => [...messages, message]);
+    });
+  }
+
+  onSendMessageClickEvent(){
+    const message = this.currentMessage().trim();
+
+    if (!message) {
+      return;
+    }
+
+    this.webSocketService.sendMessage(this.username(), message);
+    this.currentMessage.set('');
+  }
 
 }
