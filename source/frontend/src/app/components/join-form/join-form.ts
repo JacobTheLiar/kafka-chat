c0862e35a6511b1c23084998ba22916037d41beb
jakub <jacob_the_liar@tlen.pl>
2026-01-01 19:13:09 +0100

    feat(frontend): add JoinForm and ChatRoom components with Websocket service
    - Implement `JoinForm` and `ChatRoom` components with basic structure and templates.
    - Create `Websocket` service to provide injectable WebSocket functionality.

---
+++ source/frontend/src/app/components/join-form/join-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-form',
+  imports: [],
+  templateUrl: './join-form.html',
+  styleUrl: './join-form.scss',
+})
+export class JoinForm {
+
+}
