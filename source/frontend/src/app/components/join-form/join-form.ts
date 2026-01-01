68de6ec6821bbf3a12ce1ae8067c574f659b773e
jakub <jacob_the_liar@tlen.pl>
2026-01-01 20:23:58 +0100

    feat(frontend): enhance JoinForm with user input binding and join event handling
    - Add `username` and `roomName` models with two-way binding using `ngModel`.
    - Include a "Join" button triggering `onJoinClickEvent` to emit user and room data.
    - Update template with input fields and button to improve user interaction.

--- source/frontend/src/app/components/join-form/join-form.ts
+++ source/frontend/src/app/components/join-form/join-form.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, model, output} from '@angular/core';
+import {RoomData} from '../../model/room-data.model';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-join-form',
-  imports: [],
   templateUrl: './join-form.html',
   styleUrl: './join-form.scss',
+  imports: [
+    FormsModule
+  ]
 })
 export class JoinForm {
+  protected joined = output<RoomData>();
+  protected username = model('');
+  protected roomName = model('');
 
+  onJoinClickEvent(): void{
+    const username = this.username().trim();
+    const roomName = this.roomName().trim();
+
+    if (!username || !roomName) {
+      return;
+    }
+
+    this.joined.emit({ username, roomName });
+  }
 }
